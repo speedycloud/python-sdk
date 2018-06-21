a6ad7b732d7b4beac0a6d36afed2196f244317e7
iliul <liul.stone@gmail.com>
2018-06-21 19:33:17 +0800

    update object storage example
    Signed-off-by: iliul <liul.stone@gmail.com>

--- speedycloud/examples/example.py
+++ speedycloud/examples/example.py
@@ -1,9 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import speedycloud
 
-bucket_name = "wsx"
-key_name = "keyName"
-file_path = "/home/wsx/Desktop/ssss.mov"
+BUCKET = "sdk"
+CREATE_BUCKET = "new-bucket"
+OBJECT = "image.png"
+FILE_PATH = "./50Mb.file"
 
-cli = speedycloud.create_object_storage_api("FC12640FE92A7", "f2315283a18e0c7dc835ed32b4cad42e5d9206a7e0c79")
+ACCESS_KEY = "YOUR-ACCESS-KEY"
+SECRET_KEY = "YOUR-SECRET-KEY"
 
-cli.upload_big_data(bucket_name, key_name, file_path, "file", {})
+# 初始化对象存储 API Handler
+oss_api = speedycloud.create_object_storage_api(ACCESS_KEY, SECRET_KEY)
+
+# 获取 bucket 列表
+print oss_api.list(BUCKET)
+
+# 创建 bucket
+print oss_api.create_bucket(CREATE_BUCKET)
+
+# 设置 bucket acl
+print oss_api.update_bucket_acl(CREATE_BUCKET, {'x-amz-acl': "public-read"})
+
+# 查询 bucket acl
+oss_api.query_bucket_acl(CREATE_BUCKET)
+
+# 删除 bucket
+print oss_api.delete_bucket(CREATE_BUCKET)
+
+# 大文件分段上传，封装接口
+oss_api.upload_big_data(BUCKET, "50Mb.file", FILE_PATH, {'x-amz-acl': "public-read"})
